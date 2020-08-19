35a66db9f9d0a9be7b8d657af728e35cf6f49ec3
tabliang <tabliang@tencent.com>
2020-08-19 09:01:43 +0800

    完善错误页的包装

--- src/middleware/empty.ts
+++ src/middleware/empty.ts
@@ -1,3 +1,4 @@
+import { renderFile } from "https://deno.land/x/dejs@0.8.0/mod.ts";
 import { Context } from "../types.d.ts";
 
 export async function empty(ctx: Context, next: Function) {
@@ -6,8 +7,14 @@ export async function empty(ctx: Context, next: Function) {
     response,
   } = ctx;
   if (!response.body) {
-    response.body = "Not Found!";
+    const msg = "Not found";
+    ctx.logger?.error("404", msg);
     response.status = 404;
-    ctx.logger?.error('404 Not found')
+    response.body = await renderFile(`${Deno.cwd()}/views/error.ejs`, {
+      title: "Empty",
+      status: response.status,
+      message: msg,
+      stack: "",
+    });;
   }
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-import { Context as OakContext } from "https://deno.land/x/oak/mod.ts";
+import { Context as OakContext } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 import { Logger } from "./service/logger.ts";
 
 export interface Context extends OakContext {
